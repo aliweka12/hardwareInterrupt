bf18c7482660189f38242435721b822346e4c719
aliweka12 <123128594+aliweka12@users.noreply.github.com>
2023-11-30 19:11:55 +0000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# hardwareInterrupt
- 
+# STM32F4xx GPIO and Clock Management
+
+This project provides a C++ implementation for managing system clock configuration, LED operations, and external interrupts on STM32F4xx series microcontrollers. It showcases object-oriented principles in embedded programming by encapsulating functionalities in classes.
+
+## Features
+
+- **Clock Management**: Configures the system clock with appropriate PLL settings and power configurations.
+- **LED Management**: Manages LED operations including initialization and state toggling.
+- **Interrupt Management**: Sets up and handles external interrupts on a GPIO pin.
+
+## Prerequisites
+
+To use this project, you'll need:
+- STM32F4xx HAL Libraries.
+- An STM32F4xx series microcontroller.
+- A compatible development environment, such as STM32CubeIDE.
+
+## Project Structure
+
+- `ClockManager`: A class responsible for configuring the system clock.
+- `IndicatorLED`: A class for managing LED operations.
+- `InterruptManager`: A class for configuring and handling external interrupts (EXTI).
+
+## Usage
+
+1. **Initialization**: The `main` function initializes the HAL library, system clock, LED, and external interrupt handler.
+2. **Clock Configuration**: The `ClockManager` class configures the system clock.
+3. **LED Control**: Use the `IndicatorLED` class to initialize and control the LED state.
+4. **Interrupt Handling**: The `InterruptManager` class sets up and manages external interrupts.
+
---
+++ Src/main.c
@@ -0,0 +1,131 @@
+#include "main.h"
+
+/**
+ * @class ClockManager
+ * @brief Manages the system clock configuration.
+ */
+class ClockManager {
+public:
+    /**
+     * @brief Configures the system clock.
+     * @note Sets up the clock source, voltage scaling, PLL, and flash latency.
+     */
+    static void Configure() {
+        RCC_ClkInitTypeDef RCC_ClkInitStruct;
+        RCC_OscInitTypeDef RCC_OscInitStruct;
+        HAL_StatusTypeDef ret = HAL_OK;
+
+        __HAL_RCC_PWR_CLK_ENABLE();
+        __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+        RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+        RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+        RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+        RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+#if defined(USE_STM32469I_DISCO_REVA)
+        RCC_OscInitStruct.PLL.PLLM = 25;
+#else
+        RCC_OscInitStruct.PLL.PLLM = 8;
+#endif
+        RCC_OscInitStruct.PLL.PLLN = 360;
+        RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+        RCC_OscInitStruct.PLL.PLLQ = 7;
+        RCC_OscInitStruct.PLL.PLLR = 6;
+
+        ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
+        if(ret != HAL_OK) { while(1) {} }
+
+        ret = HAL_PWREx_EnableOverDrive();
+        if(ret != HAL_OK) { while(1) {} }
+
+        RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+        RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+        RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+        RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+        RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+        ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+        if(ret != HAL_OK) { while(1) {} }
+    }
+};
+
+/**
+ * @class IndicatorLED
+ * @brief Manages LED operations.
+ */
+class IndicatorLED {
+public:
+    /**
+     * @brief Initializes LED1.
+     */
+    static void Initialize() {
+        BSP_LED_Init(LED1);
+    }
+
+    /**
+     * @brief Toggles LED1 state.
+     */
+    static void ToggleState() {
+        BSP_LED_Toggle(LED1);
+    }
+};
+
+/**
+ * @class InterruptManager
+ * @brief Manages external interrupts.
+ */
+class InterruptManager {
+public:
+    /**
+     * @brief Configures EXTI line 0 (connected to PA.00 pin) in interrupt mode.
+     * @note Initializes GPIOA and configures the EXTI line for interrupt on rising edge.
+     */
+    static void Configure() {
+        GPIO_InitTypeDef GPIO_InitStructure;
+
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+
+        GPIO_InitStructure.Mode = GPIO_MODE_IT_RISING;
+        GPIO_InitStructure.Pull = GPIO_NOPULL;
+        GPIO_InitStructure.Pin = GPIO_PIN_0;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+        HAL_NVIC_SetPriority(EXTI0_IRQn, 2, 0);
+        HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+    }
+};
+
+/**
+ * @brief Main program entry point.
+ * @note Initializes the system and enters an infinite loop.
+ */
+int main() {
+    HAL_Init(); // Initialize HAL library
+    ClockManager::Configure(); // Configure system clock
+    IndicatorLED::Initialize(); // Initialize LED
+    InterruptManager::Configure(); // Configure external interrupt
+
+    while (1) {} // Infinite loop
+}
+
+/**
+ * @brief Callback function for EXTI line detection.
+ * @note Toggles the LED when EXTI line 0 is triggered.
+ * @param GPIO_Pin Specifies the pins connected to the EXTI line.
+ */
+extern "C" void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+    if (GPIO_Pin == GPIO_PIN_0) {
+        IndicatorLED::ToggleState();
+    }
+}
+
+#ifdef USE_FULL_ASSERT
+/**
+ * @brief Reports assert_param error.
+ * @param file Pointer to the source file name.
+ * @param line assert_param error line source number.
+ */
+extern "C" void assert_failed(uint8_t *file, uint32_t line) {
+    while (1) {} // Infinite loop for error handling
+}
+#endif
